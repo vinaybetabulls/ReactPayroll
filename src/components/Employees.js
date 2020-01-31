ba5718c590105824ef96ef393ec642cf1371e1ed
vinaybetabulls <bodigevinay92@gmail.com>
2020-01-31 22:59:06 +0530

    context api

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -4,11 +4,6 @@ import {Consumer} from "./contextapi";
 export default class Employees extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      employeename: "Mani",
-      name: ""
-    };
-    this.changeState = this.changeState;
     console.log("constructor Employees");
   }
   static getDerivedStateFromProps(props, state) {
@@ -18,26 +13,18 @@ export default class Employees extends Component {
   componentDidMount() {
     console.log("component did  mount Employees");
   }
-  changeState = (dispatch)=>{
-    console.log("dispatch..", dispatch)
-    dispatch({
-     type:"changestate",payload:"ngchgf"
-    })
 
-  }
   render() {
     console.log("render Employees");
-    console.log('state employee', this.state)
     return (
       <Consumer>
         {props => {
           return (
             <div>
-              {console.log(props)}
               <h1>
                 Employee name is {props.employee.employeeName} and my age is {props.employee.age}
               </h1>
-              <button onClick={()=>props.dispatch({type:'changestate',payload:{name:'vinay'}})}>chnagestate</button>
+              <button onClick={()=>props.dispatch({type:'changestate',payload:{name:'vinay', age: 27}})}>chnagestate</button>
             </div> 
           );
         }}
--- src/components/contextapi.js
+++ src/components/contextapi.js
@@ -1,46 +1,39 @@
-import React from 'react';
+import React from "react";
 const Context = React.createContext();
 
-
 export class Provider extends React.Component {
-
-  state = {
-    employee: {
-      employeeName: 'Mani',
-      age: 30
-    },
-    dispatch: (action) => {
-      let data = this.reducer(this.state, action)
-      console.log('after dispatch..', data)
-      this.setState({
-        state: {
-          employee: {
-            employeeName: data.employeeName
-          }
-        }
-      })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: {
+        employeeName: "Mani",
+        age: 30
+      },
+      dispatch: this.reducer
+    };
   }
 
-  reducer = (state, action) => {
-    console.log('reducer state....', state)
+  reducer = action => {
     switch (action.type) {
-      case 'changestate':
-        console.log(Object.assign({}, this.state.employee, { employeeName: action.payload.name, age: 25 }))
-        return Object.assign({}, this.state.employee, { employeeName: action.payload.name, age: 25 })
-
+      case "changestate":
+        this.setState({
+          employee: {
+            employeeName: action.payload.name,
+            age: action.payload.age
+          }
+        });
       default:
-        return this.state
+        return this.state;
     }
-  }
+  };
   render() {
-
-    { console.log('render.... ', this.state); }
     return (
-      <Context.Provider value={{ employee: this.state.employee, dispatch: this.state.dispatch }}>
+      <Context.Provider
+        value={{ employee: this.state.employee, dispatch: this.state.dispatch }}
+      >
         {this.props.children}
       </Context.Provider>
-    )
+    );
   }
 }
-export const Consumer = Context.Consumer; 
+export const Consumer = Context.Consumer;
